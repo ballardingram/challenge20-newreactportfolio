dca24ec5501ab87970033bf789d8fa064c9e2630
Ballard Ingram <ballardingram@outlook.com>
2022-06-26 14:00:07 -0500

    components/app - imported and exported

---
+++ bmiportfolio/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+import Skills from "./components/Skills";
+
+export default function App() {
+    return(
+        <main className="text-grey-400-bg-gray-900 body-font">
+            <About />
+            <Contact />
+            <Navbar/>
+            <Projects />
+            <Resume />
+            <Skills />
+        </main>
+    );
+}
