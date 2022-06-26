e30618ccf492837c2edef34d0d5f687975e2926c
Ballard Ingram <ballardingram@outlook.com>
2022-06-26 15:26:33 -0500

    added project media and data for components

---
+++ bmiportfolio/src/data.js
@@ -0,0 +1,51 @@
+// PROJECTS ARRAY - REQUIRED 6 FOR CHALLENGE
+export const projects = [
+    {
+        title: "80's Arcade Quiz",
+        subtitle: "JS and Web API Challenge",
+        description:
+        "An 80's style arcade quiz inspired by the movie, 'Ready Player One' and my love of coin arcades as a child.",
+        image: "../../public/project1.gif",
+        link: "https://ballardingram.github.io/challenge4-80s-quiz/",
+    },
+    {
+        title: "WeatherView Dashboard",
+        subtitle: "Server-Side API and JQuery Challenge",
+        description:
+        "A simple dashboard with some fun upgrades. You'll experience a different dashboard based on your time of day.",
+        image: "../../public/project2.gif",
+        link: "https://ballardingram.github.io/challenge6-weatherapp/",
+    },
+    {
+        title: "Superhero Wiki",
+        subtitle: "Interactive Front-End Site with 2 APIs",
+        description:
+        "Want to know more about your favorite Marvel Superheo? Visit the Superhero Wiki!",
+        image: "../../public/project3.gif",
+        link: "https://ballardingram.github.io/Super-Hero-Wiki/",
+    },
+    {
+        title: "Note Taker",
+        subtitle: "Node and Express.JS Challenge",
+        description:
+        "A note-taker site that allows you to add, update, and delete tasks!",
+        image: "../../public/project4.gif",
+        link: "https://challenge11-notetaker.herokuapp.com/",
+    },
+    {
+        title: "{COD}EZ.community",
+        subtitle: "Interactive Full Stack Project",
+        description:
+        "Where the best of the best come to share their tips from experiences in the world of coding. Do you know something cool, smart, and helpful? Start sharing tips today!",
+        image: "../../public/project5.gif",
+        link: "https://cod-ez-community.herokuapp.com/api/tips",
+    },
+    {
+        title: "PWA Budget Tracker",
+        subtitle: "PWA Challenge",
+        description:
+        "A budget tracking application that has offline and online functionality.",
+        image: "../../public/project6.gif",
+        link: "https://fast-fortress-82643.herokuapp.com/",
+    },
+]
