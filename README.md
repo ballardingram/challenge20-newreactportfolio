2e75aa9a7765f321aaaabe07208ff13e3402bcd0
Ballard Ingram <ballardingram@outlook.com>
2022-07-05 15:05:06 -0500

    readme and slight change to about wording

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # New React Portfolio
-App created, working on deployment.
+This is part of Challenge 20 - React Portfolio for my UT Bootcamp Full Stack Program.
+
+## Built With
+This application was built with React and Tailwind. There was no starter code.
+
+## Contributions
+This was largely an independent project. I worked iwth my Tutor on the GH Pages deployment after I received a few 404 page errors. This is the 2nd time that I used Tailwind to create a project and relied on their documentation to get this done.
+
+## Opportunities
+Aside from the 404 error, I really did not have a lot of opportunities with this code. I enjoyed REACT and how the pages routed together.
+
+## Future Development
+I want to make my contact me page fully functional so I can use this as an actual website. I would also like to carosel my projects so when I add them my site it doesn't have "clunky" formatting.
+
+## Project Media
+https://ballardingram.github.io/challenge20-newreactportfolio/
--- src/components/About.js
+++ src/components/About.js
@@ -9,13 +9,13 @@ export default function About() {
                    About Me
                 </h1>
                 <p className="lg:w-2/3 mx-auto leading-relaxed text-xl" id="navfont">
-                    Howdy! I'm a full-stack web developer located in Austin, Texas! Surprisingly enough, I'm actually from Chicago, IL. I still haven't caught on to the 'ya'll' thing yet. If you bring me on to your team be ready for me to say, 'did yous guys fix that bug over'der?' Yeah, I know, can't take the city out of the Man. I have been living in Texas off and on since 2005. I tied the knot, adopted a few boys, and settled my roots in Texas. This is going to be home for a while. What's not to love?Like the city of Chicago my goal with learning code is to create something magnificent. Strong layouts, responsive design, and a polished UX.
+                    Howdy! I'm a full-stack web developer located in Austin, Texas! Surprisingly enough, I'm actually from Chicago, IL. I still haven't caught on to the 'ya'll' thing yet. If you bring me on to your team be ready for me to say, 'did yous guys fix that bug over'der?' Yeah, I know, can't take the city out of the Man. I have been living in Texas off and on since 2005. I tied the knot, adopted a few boys, and settled my roots in Texas. This is going to be home for a while. What's not to love?
                 </p>
                 <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 mt-4 text-zinc-900 tracking-widest" id="sectionfont">
                    My Vision
                 </h1>
                 <p className="lg:w-2/3 mx-auto leading-relaxed text-xl" id="navfont">
-                    Like the city of Chicago my goal with learning code is to create something magnificent with strong layouts, responsive design, and a polished UX. My perspective is 'if you can dream it, then make it a reality.' In my years of professional experience I've seen opportunities present themselves to me that I could not take advantage of because I didn't have the tools to make them a reality. In 2022 - that all changed.
+                    Like the city of Chicago my goal with learning code is to create something magnificent with strong layouts, responsive design, and a polished UX. My perspective is 'if you can dream it, then make it a reality.' In my years of professional experience I've seen opportunities present themselves to me that I could not take advantage of because I didn't have the tools to make them a reality. In 2022 - that all changed. I'm working on projects to create new businesses, tools, and improvements for consumers and businesses.
                 </p>
             </div>
         </div>
